23f3d7f1a3650dcc5a8ce8cbc73d5a71d4389933
Matthew Heinsen Egan <matthew.heinsen.egan@gmail.com>
2017-03-18 16:45:48 +0900

    Update versions for Appveyor.

--- scripts/appveyor/build.sh
+++ scripts/appveyor/build.sh
@@ -2,4 +2,4 @@ cd "/c/projects/llvm_build"
 make -j4
 make install
 cd $APPVEYOR_BUILD_FOLDER
-7z a llvm_install_3.9.0.zip "C:\\projects\\llvm_install\\*"
+7z a llvm_install_4.0.0.zip "C:\\projects\\llvm_install\\*"
